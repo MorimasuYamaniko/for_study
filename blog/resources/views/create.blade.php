1d7d0a5620362b38432f0e512498a2ccf0e4aabb
MorimasuYamaniko <i160573a@yokohama-cu.ac.jp>
2021-04-06 20:08:26 +0900

    commit

--- blog/resources/views/create.blade.php
+++ blog/resources/views/create.blade.php
@@ -3,13 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>create</title>
+        <title>Blog</title>
         <!-- Fonts -->
         <link href="https:/fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" href="/css/app.css">
     </head>
-    
     <body>
+        <h1>Blog Name</h1>
         <form action="/posts" method="POST">
             @csrf
             <div class="title">
@@ -18,7 +18,7 @@
             </div>
             <div class="body">
                 <h2>Body</h2>
-                <textarea name="post[body]" placeholder="こちらは本文です。"/>
+                <textarea name="post[body]" placeholder="仮のテキスト"></textarea>
             </div>
             <input type="submit" value="保存"/>
         </form>
---
+++ blog/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function() {
+//   return view('index'); 
+// });
+
+Route::get('/', 'PostController@index');
+
+Route::get('/posts/create', 'PostController@create');
+
+Route::get('/posts/{post}', 'PostController@show');
+// なんか知らんけど、Route::get('/posts/{post}', 'PostController@show');
+// の/posts/{post}の{}があいまいらしい。数字でも文字列でも{}以降は判別しないらしい
+// だからとりあえず一番最後に書いてくださいとのこと。
